cc8539d29d20aa68ad53dd6eb9bcda0fa0d6377c
Jeong, Jiung <ethernuiel@sanultari.com>
2014-01-02 01:43:36 +0900

    add mac address grain with dependencies

--- salt/roots/_grains/hello.py
+++ salt/roots/_grains/hello.py
@@ -1,2 +1,15 @@
+import netifaces as nif
+
 def hello():
-    return {'hello': 'world', 'goodbye': 'blue'}
+    return {
+            'hello': 'world', 
+            'goodbye': 'blue',
+            'mac_addresses': mac()
+            }
+
+def mac():
+    macs = {}
+    for iface in nif.interfaces():
+        macs[iface] = nif.ifaddresses(iface)[nif.AF_LINK][0]['addr']
+
+    return macs
--- salt/roots/prerequisites/init.sls
+++ salt/roots/prerequisites/init.sls
@@ -4,9 +4,20 @@
 python-pip:
   pkg:
     - installed
+
+python-dev:
+  pkg:
+    - installed
       
 pymongo:
   pip:
     - installed
     - require:
       - pkg: python-pip
+
+netifaces:
+  pip:
+    - installed
+    - require:
+      - pkg: python-pip
+      - pkg: python-dev
